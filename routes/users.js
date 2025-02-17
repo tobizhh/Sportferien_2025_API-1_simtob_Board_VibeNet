1874b0988d975b791e9f7abcbb476ca28c4ade4b
tobizhh <grrtiprrti@gmail.com>
2025-02-17 16:04:11 +0100

    urls fixxed

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const User = require("../models/User");
-const requireAuth = require("../Middleware/authMiddleware");
+const requireAuth = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
