47b5d157c6ad5866f3cb605bfa9a43e6c43f198f
tobizhh <grrtiprrti@gmail.com>
2025-02-17 15:18:48 +0100

    casefix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/friends" element={userId ? <Friends userId={userId} /> : <Navigate to="/login" />} />
