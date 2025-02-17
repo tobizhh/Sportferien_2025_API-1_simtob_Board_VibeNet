16b49458ddb18d67e3882b5c7f81085ef15b7406
tobizhh <grrtiprrti@gmail.com>
2025-02-17 16:15:25 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "./", // 🔹 Ensures correct relative paths for Netlify
+  base: "/", // 🔹 Ensures correct relative paths for Netlify
   plugins: [react()],
 });
