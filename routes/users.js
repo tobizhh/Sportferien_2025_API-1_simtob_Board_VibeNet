10dfe1392414cda9ff6f2cea7b80d8efdcf3caf8
tobizhh <grrtiprrti@gmail.com>
2025-02-18 14:25:20 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const User = require("../models/User");
-const requireAuth = require("../middleware/authMiddleware");
+const requireAuth = require("../Middleware/authMiddleware");
 
 const router = express.Router();
 
