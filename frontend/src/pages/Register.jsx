31df4f0a055798735168697c57b65459f96bb6c6
tobizhh <grrtiprrti@gmail.com>
2025-02-18 14:32:57 +0100

    new urls

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -15,7 +15,7 @@ function Register() {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/api/auth/register", { 
+      await axios.post("https://sportferien-2025-api-1-simtob-board.onrender.com/api/auth/register", { 
         username: username.trim(), 
         email: email.trim(), 
         password: password.trim(),
